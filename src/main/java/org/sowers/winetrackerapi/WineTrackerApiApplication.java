5224625dda5b2ac9a241eb7353cbd2215434f969
Sabriya Sowers <sabriyasowers@gmail.com>
2024-04-21 18:33:35 -0500

    Initial commit

---
+++ src/main/java/org/sowers/winetrackerapi/WineTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package org.sowers.winetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sowers/winetrackerapi/WineTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sowers.winetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
